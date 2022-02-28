a400f3b9ff45110ddbc1586665f088b037b27045
Vincent M <vincentmartins52@yahoo.fr>
2022-02-28 18:45:13 +0100

    installation express

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,7 @@
-console.log("voilà du code");
+const http = require("http");
+const app = require("./app");
+
+app.set("port", process.env.PORT || 3000);
+const server = http.createServer(app);
+
+server.listen(process.env.PORT || 3000);
