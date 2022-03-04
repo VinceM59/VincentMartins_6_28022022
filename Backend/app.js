e6db2973019c5c07e31904222e225a0113c349f3
Vincent M <vincentmartins52@yahoo.fr>
2022-03-04 16:55:54 +0100

    Création fichiers user

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const sauceRoutes = require("./routes/sauce");
+const userRoutes = require("./routes/user");
 
 mongoose
   .connect(
@@ -29,6 +30,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 app.use("/api/sauce", sauceRoutes);
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
 
