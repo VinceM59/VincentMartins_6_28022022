6c9141e1d799e3ff5ce4ef6b105d16b92a50170e
Vincent M <vincentmartins52@yahoo.fr>
2022-03-07 12:55:00 +0100

    Création auth et multer-config

--- Backend/routes/sauce.js
+++ Backend/routes/sauce.js
@@ -2,12 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
+const auth = require("../middleware/auth");
+
 const sauceCtrl = require("../controllers/sauce");
 
-router.get("/", sauceCtrl.getAllSauce);
-router.post("/", sauceCtrl.createSauce);
-router.get("/:id", sauceCtrl.getOneSauce);
-router.put("/:id", sauceCtrl.modifySauce);
-router.delete("/:id", sauceCtrl.deleteSauce);
+router.get("/", auth, sauceCtrl.getAllSauce);
+router.post("/", auth, sauceCtrl.createSauce);
+router.get("/:id", auth, sauceCtrl.getOneSauce);
+router.put("/:id", auth, sauceCtrl.modifySauce);
+router.delete("/:id", auth, sauceCtrl.deleteSauce);
 
 module.exports = router;
